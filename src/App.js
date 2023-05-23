0478df0e01a84c602a6c8552c7bada5c69cff7f0
Sanjiro1202 <santiagostar58@gmail.com>
2023-05-22 19:35:26 -0500

    Instalación de Esri Loader - App Incompleta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { loadModules } from 'esri-loader';
 
-function App() {
+const MapaBogota = () => {
+  const mapRef = useRef(null);
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  useEffect(() => {
+    // Carga los módulos necesarios de ArcGIS
+    loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
+      .then(([Map, MapView]) => {
+        const map = new Map({
+          basemap: 'streets' // Puedes cambiar el tipo de mapa aquí
+        });
+
+        const view = new MapView({
+          container: mapRef.current,
+          map: map,
+          center: [-74.046, 4.676], // Coordenadas del edificio de ESRI Colombia en el Chicó
+          zoom: 15
+        });
+
+        view.when(() => {
+          setMapLoaded(true);
+        });
+      })
+      .catch((error) => {
+        console.error('Error al cargar el mapa:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {mapLoaded ? (
+        <div
+          ref={mapRef}
+          style={{ width: '100%', height: '600px' }}
+        ></div>
+      ) : (
+        <div>Cargando el mapa...</div>
+      )}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mapa de Bogotá</h1>
+      <MapaBogota />
+    </div>
+  );
+};
 
 export default App;
